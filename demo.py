8be49740e12ed413ca78451b5889dafef23d84c8
OuOLeaf <chandelier0207@gmail.com>
2022-10-21 14:04:47 +0800

    v221021
    plotly solves labels in chinese

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import ecdf
-
+def fun():
+    st.write('fun click')
+    return 0
 def main():
     st.markdown("""
 <style>
@@ -10,13 +12,18 @@ def main():
 .title-font {
     font-size:30px !important;
 }</style>""", unsafe_allow_html=True)
-    former = st.sidebar.slider('前屆答對題數', 1, 110, 30)
-    st.markdown('<p class="title-font">Demo version</p>', unsafe_allow_html=True)
-    [fig, corr] = ecdf.equalize_pltly(former)
-    st.write(fig)
-   
-    st.markdown(f'<p class="big-font">對應到應屆的答對題數為 {corr} 題</p>', unsafe_allow_html=True)
-   
+    a = st.file_uploader("前屆試題檔")
+    b = st.file_uploader("應屆試題檔")
+    c = st.file_uploader("共同題檔")
+    agree = st.checkbox('執行')
+    if a and b and c and agree:
+        former = st.sidebar.slider('前屆答對題數', 1, 110, 30)
+        st.markdown('<p class="title-font">Demo version</p>', unsafe_allow_html=True)
+        [fig, corr] = ecdf.equalize_pltly(former)
+        st.write(fig)
+        st.markdown(f'<p class="big-font">對應到應屆的答對題數為 {corr} 題</p>', unsafe_allow_html=True)
+    else:
+        st.write("請放入資料")
             
 
 
---
+++ move.py
@@ -0,0 +1,17 @@
+
+import os
+import glob
+import shutil
+
+hPath = r'D:\Dropbox\Andy\streamlit\finanical_standard'
+gPath = r'C:\Users\user\Documents\GitHub\finan_std'
+dfPath = glob.glob(gPath + "/*.py")
+
+for i in dfPath:
+    os.remove(i)  
+mfPath = glob.glob(hPath + "/*.py")
+for j in mfPath:
+    file = '\\' + j.split('\\')[-1]
+    # print(file)
+    shutil.copy(hPath + file, gPath + file)
+
